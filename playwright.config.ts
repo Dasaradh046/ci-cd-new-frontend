4acfb53eb3a065c01ce1fbea6e47d7412f510248
KDasaradh <kdasaradha525@gmail.com>
2026-03-05 16:42:33 +0530

    chore: fixed playwright and github repo username lower case issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'bun run dev',
+    command: 'pnpm build && pnpm start',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
